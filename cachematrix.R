85d8b735358eaa9834017b8245a76e99cc45b18e
Jason Chionh <jasonchionh@gmail.com>
2015-02-21 17:42:59 +0800

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## we have 2 functions here
+# makeCacheMatrix is a function that makes special object 
+# that has accessor functions to get set the original matrix
+# and get set the inverse matrix
+
+# cacheSolve takes in the special object created by makeCacheMatrix
+# it first checks if the object already has a precomputed inverse
+# if yes, it'll just use it
+# if no, it'll compute the inverse, store it, and then returns it
+
 ## Write a short comment describing this function
 # This function simply creates a list of functions that can
 # cache a matrix and the inverse.
@@ -44,11 +54,15 @@ makeCacheMatrix <- function(mat = matrix()) {
 # makeCahceMatrix
 # if the inverse of the matrix is already, set, we just return it
 # else, we calculate the matrix then store it, and return it
+# IMPORTANT: if the inverse is already cache, we'll just TRUST
+# that the inverse is correct.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  
   # let's first check x to see if we already have an inverse
   # here, we TURST that the cached inverse is correct
   invMat <- x$getInverse()
+  
   # if we've already got a value, use it
   if (!is.null(invMat)) {
     message("using cached value")
@@ -59,6 +73,7 @@ cacheSolve <- function(x, ...) {
   # compute it
   origMat <- x$get()
   invMat <- solve(origMat) %*% origMat
+  
   # cache it
   x$setInverse(invMat)
   
