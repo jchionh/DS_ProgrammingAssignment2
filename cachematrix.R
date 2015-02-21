8a164c8f71093edd764d91bce78b8a5faf730cbf
Jason Chionh <jasonchionh@gmail.com>
2015-02-21 17:27:54 +0800

    changed input param to mat

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,7 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(mat = matrix()) {
   # set the cached inverse to null
   cachedInvMatrix <- NULL
   
